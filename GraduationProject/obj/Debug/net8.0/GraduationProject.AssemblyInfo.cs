a1010898e68c90949c36118d6e26f325c4e8e0bf
Mohamed Sayid <m.alkordy2003@gmail.com>
2025-01-24 22:21:11 +0200

    class1

--- GraduationProject/obj/Debug/net8.0/GraduationProject.AssemblyInfo.cs
+++ GraduationProject/obj/Debug/net8.0/GraduationProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GraduationProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50bbc8d0a69a7412223195c0c90d070b7e71bdb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GraduationProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GraduationProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
